2a4a22a4950f8f91dc5e7662ad4524c7161508f6
Gaurav Ashok Chavhan <gchavan65@gmail.com>
2021-08-19 09:34:47 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [num, updatenum] = useState();
+  const [userList, setUserList] = useState([]);
+
+
+  useEffect(() => {
+    const getdata = async () => {
+      const result = await axios.get(`https://reqres.in/api/users?page=${num}`)
+      setUserList(result.data.data);
+      // console.log(result.data.data);
+    }
+
+    getdata()
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+    <div className="container-fluid p-3">
+
+      <div className='bg-dark'>
+        <select value={num} onChange={(event) => {
+          updatenum(event.target.value);
+        }}>
+          <option value='1'>1</option>
+          <option value='2'>2</option>
+        </select><h1 className='text-center text-light'>Page No:{num}</h1>
+      </div>
+
+      <table className="table table-sm mt-3">
+        <thead className="thead-dark">
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Images</th>
+        </thead>
+        <tbody>
+          {userList.map((val, ind) => <tr key={ind}>
+            <td>{val.first_name}</td>
+            <td>{val.last_name}</td>
+            <td>{val.email}</td>
+            <td><img src={val.avatar} width="70" height="70" alt="No img" /></td>
+          </tr>)}
+        </tbody>
+      </table>
+
     </div>
   );
 }
 
 export default App;
+
+
